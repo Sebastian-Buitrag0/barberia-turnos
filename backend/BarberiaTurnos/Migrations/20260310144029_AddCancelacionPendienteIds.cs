f77567295e70ecec000325828a6f195a94d8b7b1
Sebastian-Buitrag0 <sebastian.buitragoayala@gmail.com>
2026-03-10 09:46:47 -0500

    feat: add CancelacionPendienteIds column to the Turnos table.

---
+++ backend/BarberiaTurnos/Migrations/20260310144029_AddCancelacionPendienteIds.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarberiaTurnos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCancelacionPendienteIds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CancelacionPendienteIds",
+                table: "WhatsAppStates",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CancelacionPendienteIds",
+                table: "WhatsAppStates");
+        }
+    }
+}
